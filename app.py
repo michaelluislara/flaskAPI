d30df21135c466ebe33fe7c81e0e334767dd3f10
michaelluislara <michael.luis.lara@gmail.com>
2023-02-09 20:09:25 +0000

    added key working with env var.

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask_api import FlaskAPI
+from flask import request
+import os
 import giver
 
 app = FlaskAPI(__name__)
+
+API_KEY = os.environ['API_KEY']
 
 @app.route('/')
 def example():
     """
     Helper function to send the data to the requestor.
     """
-    return giver.givedata()
+    api_key = request.headers.get("Authorization")
+    if api_key == API_KEY:
+        return giver.givedata()
+    else:
+        return "Unauthorized", 401
 if __name__ == "__main__":
    # run the flask app
    app.run()
